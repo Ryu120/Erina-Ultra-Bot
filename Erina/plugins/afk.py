05dfa8a0e30e39d0c43559ba6f6c4aa701a8b9f5
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2022-11-12 19:09:12 +0530

    Update afk.py

--- Erina/plugins/afk.py
+++ Erina/plugins/afk.py
@@ -16,8 +16,45 @@ from pyrogram.types import Message
 
 from Erina import bot as app
 from Erina.Database import add_afk, is_afk, remove_afk
-from Erina.helpers import get_readable_time, put_cleanmode
+from Erina.helpers import put_cleanmode
 
+from typing import Union
+from datetime import datetime, timedelta
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
+cleanmode = {}
+
+async def put_cleanmode(chat_id, message_id):
+    if chat_id not in cleanmode:
+        cleanmode[chat_id] = []
+    time_now = datetime.now()
+    put = {
+        "msg_id": message_id,
+        "timer_after": time_now + timedelta(minutes=5),
+    }
+    cleanmode[chat_id].append(put)
 
 @app.on_message(filters.command(["afk", f"afk@ErinaGroupbot"]))
 async def active_afk(_, message: Message):
